7bd2ab851b6c540ed9177c3997ce98f01ff5ab11
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2026-08-18 14:12:12 +0200

    Version 7.331.1
    Diesen Text hat ein KI-Agent in meinem Namen geschrieben. Ich weiß, dass das
    problematisch ist.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vutuv.MixProject do
   def project do
     [
       app: :vutuv,
-      version: "7.330.0",
+      version: "7.331.1",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
